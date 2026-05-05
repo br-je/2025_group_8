be97bbb392d99be774eea7965ad8aa0386fa424a
pkular04 <priya.kular04@gmail.com>
2026-05-05 13:23:21 +0100

    update comments ModelPartlist.h

--- ModelPartList.h
+++ ModelPartList.h
@@ -1,11 +1,11 @@
-/**     @file ModelPartList.h
-  *
-  *     EEEE2076 - Software Engineering & VR Project
-  *
-  *     Template for model part list that will be used to create the trewview.
-  *
-  *     P Evans 2022
-  */
+/**
+ * @file ModelPartList.h
+ * @brief Defines the ModelPartList class used as the Qt model for displaying
+ *        model parts in a tree view.
+ *
+ * This class manages the hierarchy of ModelPart objects and provides the
+ * required interface for Qt's QTreeView.
+ */
   
 #ifndef VIEWER_MODELPARTLIST_H
 #define VIEWER_MODELPARTLIST_H
@@ -21,89 +21,114 @@
 
 class ModelPart;
 
+
+/**
+ * @brief Model class used by Qt to display ModelPart objects in a tree view.
+ *
+ * Implements QAbstractItemModel to provide hierarchical data for the GUI.
+ */
 class ModelPartList : public QAbstractItemModel {
     Q_OBJECT        /**< A special Qt tag used to indicate that this is a special Qt class that might require preprocessing before compiling. */
+
 public:
-    /** Constructor
-      *  Arguments are standard arguments for this type of class but are not used in this example.
-      * @param data is not used
-      * @param parent is used by the parent class constructor
-      */
+
+    /**
+     * @brief Constructor
+     * @param data Initial data (not heavily used)
+     * @param parent Parent QObject
+     */
     ModelPartList( const QString& data, QObject* parent = NULL );
 
-    /** Destructor
-      *  Frees root item allocated in constructor
-      */
+    /**
+     * @brief Destructor
+     */
     ~ModelPartList();
 
-    /** Return column count
-      * @param parent is not used
-      * @return number of columns in the tree view - "Part" and "Visible", i.e. 2 in this case
-      */
+    /**
+     * @brief Returns number of columns in the model.
+     * @param parent Parent index (unused)
+     * @return Number of columns (e.g. Part, Visible)
+     */
     int columnCount( const QModelIndex& parent ) const;
 
-    /** This returns the value of a particular row (i.e. the item index) and 
-      *  columns (i.e. either the "Part" or "Visible" property).
-      *  It is used by QT internally - this is how Qt retrieves the text to display in the TreeView
-      * @param index in a stucture Qt uses to specify the row and column it wants data for
-      * @param role is how Qt specifies what it wants to do with the data
-      * @return a QVariant which is a generic variable used to represent any Qt class type, in this case the QVariant will be a string
-      */
+    /**
+     * @brief Returns data for a given index and role.
+     * @param index Specifies row and column
+     * @param role Specifies how data is used (display/edit)
+     * @return Data value as QVariant
+     */
     QVariant data( const QModelIndex& index, int role ) const;
 
-    /** Standard function used by Qt internally.
-      * @param index in a stucture Qt uses to specify the row and column it wants data for
-      * @return a Qt item flags
-      */
+    /**
+     * @brief Returns flags for an item.
+     * @param index Item index
+     * @return Qt item flags (e.g. selectable, editable)
+     */
     Qt::ItemFlags flags( const QModelIndex& index ) const;
 
 
-    /** Standard function used by Qt internally.
-      */
+    /**
+     * @brief Returns header data for the model.
+     * @param section Column index
+     * @param orientation Horizontal/vertical
+     * @param role Data role
+     * @return Header text
+     */
     QVariant headerData( int section, Qt::Orientation orientation, int role ) const;
 
 
-    /** Get a valid QModelIndex for a location in the tree (row is the row in the tree under "parent"
-      * or under the root of the tree if parent isnt specified. Column is either 0 = "Part" or 1 = "Visible" 
-      * in this example 
-      * @param row is the item index
-      * @param column is 0 or 1 - part name or visible stringstream
-      * @param parent where the row is referenced from, usually the tree root
-      * @return the QModelIndex structure
+    /**
+     * @brief Creates an index for a given row and column.
+     * @param row Row index
+     * @param column Column index
+     * @param parent Parent index
+     * @return QModelIndex for the item
      */
     QModelIndex index( int row, int column, const QModelIndex& parent ) const;
 
 
-    /** Take a QModelIndex for an item, get a QModel Index for its parent
-      * @param index of item
-      * @return index of parent
-      */
+    /**
+     * @brief Returns parent index of a given item.
+     * @param index Child index
+     * @return Parent QModelIndex
+     */
     QModelIndex parent( const QModelIndex& index ) const;
 
-    /** Get number of rows (items) under an item in tree
-      *  @param is the parent, all items under this will be counted
-      *  @return number of children
-      */
+    /**
+     * @brief Returns number of child rows under a parent.
+     * @param parent Parent index
+     * @return Number of child items
+     */
     int rowCount( const QModelIndex& parent ) const;
 
-    /** Get a pointer to the root item of the tree
-      * @return the root item pointer
-      */
+    /**
+     * @brief Returns pointer to root item.
+     * @return Root ModelPart pointer
+     */
     ModelPart* getRootItem();
 
-    /** Append a new child item to the given parent node in the tree.
-      * @param parent is the index of the parent item to add under
-      * @param data is a list of column values for the new item (name and visibility)
-      * @return the QModelIndex of the newly created item
-      */
+    /**
+     * @brief Adds a child item to the tree.
+     * @param parent Parent index
+     * @param data Data for new child
+     * @return QModelIndex of new item
+     */
     QModelIndex appendChild( QModelIndex& parent, const QList<QVariant>& data );
 
 
-	///** Remove a child item from the tree
+	/**
+     * @brief Removes rows from the model.
+     * @param row Starting row
+     * @param count Number of rows to remove
+     * @param parent Parent index
+     * @return True if removal successful
+     */
     bool removeRows(int row, int count, const QModelIndex& parent = QModelIndex()) override;
 
 
 private:
+
+    /** Pointer to the root item of the tree */
     ModelPart *rootItem;    /**< This is a pointer to the item at the base of the tree */
 };
 #endif
